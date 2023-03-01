ed13171fc124111aea8ce055a4cf90b523a41759
Asikur <asikurr817@gmail.com>
2023-03-01 14:39:59 +0600

    API Interface Added

--- app/src/main/java/com/darkzen/exploremovies/Service/Network/ApiServices.java
+++ app/src/main/java/com/darkzen/exploremovies/Service/Network/ApiServices.java
@@ -1,2 +1,12 @@
-package com.darkzen.exploremovies.Service.Network;public interface ApiServices {
+package com.darkzen.exploremovies.Service.Network;
+
+import com.darkzen.exploremovies.Service.Model.MovieModelClass;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiServices {
+
+    @GET("/3/movie/top_rated?api_key=5ac63e5fb15c28d0caa4bc2f56f62b68&language=en-US&page=1")
+    Call<MovieModelClass> getTopratedMovieList();
 }
