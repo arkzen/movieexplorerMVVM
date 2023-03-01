7a11beca131a67e30983eb3cde0c8f559a992565
Asikur <asikurr817@gmail.com>
2023-03-01 14:28:50 +0600

    Retrofit instanse class created

---
+++ app/src/main/java/com/darkzen/exploremovies/Service/Network/ApiServices.java
@@ -0,0 +1,2 @@
+package com.darkzen.exploremovies.Service.Network;public interface ApiServices {
+}
---
+++ app/src/main/java/com/darkzen/exploremovies/Service/Network/RetrofitInstanse.java
@@ -0,0 +1,23 @@
+package com.darkzen.exploremovies.Service.Network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstanse {
+
+    public static String BASE_URL = "https://api.themoviedb.org/";
+    private static Retrofit retrofit;
+
+    public static Retrofit getRetrofitInstanse() {
+
+        if (retrofit == null) {
+            retrofit=new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
+
+
