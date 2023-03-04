b9bc972152177285362c9fa0d1da1075862ee974
Asikur <asikurr817@gmail.com>
2023-03-04 14:11:23 +0600

    Viewnodel class updated

--- app/src/main/java/com/darkzen/exploremovies/ViewModel/MovieListViewmodel.java
+++ app/src/main/java/com/darkzen/exploremovies/ViewModel/MovieListViewmodel.java
@@ -1,2 +1,25 @@
-package com.darkzen.exploremovies.ViewModel;public class MovieListViewmodel {
+package com.darkzen.exploremovies.ViewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.darkzen.exploremovies.Service.Model.Result;
+import com.darkzen.exploremovies.Service.Repository.MovieRepository;
+
+import java.util.List;
+
+public class MovieListViewmodel extends AndroidViewModel {
+
+    MovieRepository mRepo;
+    public MovieListViewmodel(@NonNull Application application) {
+        super(application);
+        mRepo=MovieRepository.getInstance(application);
+    }
+
+    public LiveData<List<Result>> getTopratedMovieList(){
+        return mRepo.getTopRatedMovielist();
+    }
 }
